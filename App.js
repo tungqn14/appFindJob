2cb1f2444d8eb3a541776c8d8c586ae21aa4a52e
shin419 <ducdc.bst@gmail.com>
2021-12-05 09:56:23 +0700

    fix navigator\

--- App.js
+++ App.js
@@ -9,8 +9,8 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeNavigation from './navigation/HomeNavigation';
 import ListJobNavigation from './navigation/ListJobNavigation';
 import Home from './screens/Home';
@@ -22,54 +22,75 @@ import CompanyNavigation from './navigation/CompanyNavigation';
 const Tab = createBottomTabNavigator();
 
 const App = () => {
- // const Stack = createStackNavigation();
+  // const Stack = createStackNavigation();
 
   return (
     <NavigationContainer>
-   
       <Tab.Navigator
-            screenOptions={{
-          backgroundColor: "#9AC4F8",
-        tabBarActiveTintColor: '#F4446B',
-        tabBarLabelStyle:{fontSize:14}
-      }} >
-       <Tab.Screen name="HomeNavigation" component={HomeNavigation} 
-            options={ {header:()=>{null},
-            tabBarLabel:"Trang Chủ",
-            tabBarIcon:({color})=><Icon name="home" size={20} color="black" color={color}/>
-              }}
-          />
-       <Tab.Screen name="ListJobNavigation" component={ListJobNavigation}
-                    options={ {
-                    tabBarLabel:"Bài Viết",
-                    header:()=>{null},
-                    tabBarIcon:({color})=><Icon name="newspaper" size={20} color="black" color={color} />
-                }}/>
-                 <Tab.Screen name="CompanyNavigation" component={CompanyNavigation}
-                    options={ {
-                    tabBarLabel:"Công ty",
-                    header:()=>{null},
-                    tabBarIcon:({color})=><Icon name="industry" size={20} color="black" color={color} />
-                }}/>
-                <Tab.Screen name="AccountNavigation" component={AccountNavigation}
-                    options={ {
-                    tabBarLabel:"Tài khoản",
-                    headerTitleAlign:"center",
-                    headerTitle:"Tài Khoản",
-                    tabBarIcon:({color})=><Icon name="user" size={20} color="black" color={color} />
-                }}/>
-              
+        screenOptions={{
+          backgroundColor: '#9AC4F8',
+          tabBarActiveTintColor: '#F4446B',
+          tabBarLabelStyle: {fontSize: 14},
+        }}>
+        <Tab.Screen
+          name="HomeNavigation"
+          component={HomeNavigation}
+          options={{
+            header: () => {
+              null;
+            },
+            tabBarLabel: 'Trang Chủ',
+            tabBarIcon: ({color}) => (
+              <Icon name="home" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ListJobNavigation"
+          component={ListJobNavigation}
+          options={{
+            tabBarLabel: 'Bài Viết',
+            header: () => {
+              null;
+            },
+            tabBarIcon: ({color}) => (
+              <Icon name="newspaper" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="CompanyNavigation"
+          component={CompanyNavigation}
+          options={{
+            tabBarLabel: 'Công ty',
+            header: () => {
+              null;
+            },
+            tabBarIcon: ({color}) => (
+              <Icon name="industry" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="AccountNavigation"
+          component={AccountNavigation}
+          options={{
+            tabBarLabel: 'Tài khoản',
+            headerTitleAlign: 'center',
+            headerTitle: 'Tài Khoản',
+            tabBarIcon: ({color}) => (
+              <Icon name="user" size={20} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
-  </NavigationContainer>
-        
-   
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-  
+    flex: 1,
   },
 });
 
--- navigation/HomeNavigation.js
+++ navigation/HomeNavigation.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import Home from '../screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DetailCompany from '../screens/DetailCompany';
 import DetailPost from '../screens/DetailPost';
 import ManageAccount from '../screens/ManageAccount';
 import Account from '../screens/Account';
-  
- function HomeNavigation(){
-    return (
+const Stack = createNativeStackNavigator();
+
+function HomeNavigation() {
+  return (
     <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home}  options={
-                {
-                     header:()=>{null},
-                    // headerTitle:"Tài Khoản",
-                    // headerTitleAlign:"center"
-                }
-                }/>
-                <Stack.Screen name="DetailCompany" component={DetailCompany}  options={
-                {
-                    headerTitle:"Chi tiết công ty",
-                   
-                }
-                }/>
-                <Stack.Screen name="DetailPost" component={DetailPost}  options={
-                {
-                    headerTitle:"Chi tiết bài viết",
-                   
-                }
-                }/>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          header: () => {
+            null;
+          },
+          // headerTitle:"Tài Khoản",
+          // headerTitleAlign:"center"
+        }}
+      />
+      <Stack.Screen
+        name="DetailCompany"
+        component={DetailCompany}
+        options={{
+          headerTitle: 'Chi tiết công ty',
+        }}
+      />
+      <Stack.Screen
+        name="DetailPost"
+        component={DetailPost}
+        options={{
+          headerTitle: 'Chi tiết bài viết',
+        }}
+      />
     </Stack.Navigator>
-    )
+  );
 }
 export default HomeNavigation;
